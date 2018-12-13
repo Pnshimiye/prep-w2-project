434ca2703b44564534ee1bc34c60cbe424fe5c95
Pnshimiye <pnshimiye@gmail.com>
2018-12-13 15:05:19 +0200

    create head for html and link css and Js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var sideA = document.getElementByIdsideA
+var sideB=(parseInt(prompt("enter number for side B")))
+var sideC=(parseInt(prompt("enter number for side C")))
+
+var sideAsideB = (parseInt(prompt("enter number for side A")))+(parseInt(prompt("enter number for side B")))
+var sideAsideC = (parseInt(prompt("enter number for side A")))+ (parseInt(prompt("enter number for side C")))
+var sideBsideC =(parseInt(prompt("enter number for side B"))) + (parseInt(prompt("enter number for side C")))
