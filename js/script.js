d5cf35ac7d41e11552f083647c1aa9ec2814cffc
Pnshimiye <pnshimiye@gmail.com>
2018-12-17 00:13:35 +0200

    add a  title to the HTML page

--- js/script.js
+++ js/script.js
@@ -9,24 +9,25 @@ function makeTriangle()
   triangle[1]=parseInt(document.getElementById("sideB").value);
   triangle[2]=parseInt(document.getElementById("sideC").value);
 
-    if(( triangle[0]+ triangle[1])> triangle[2] && ( triangle[1] +  triangle[2])> triangle[0] && ( triangle[0] +  triangle[2])> triangle[1])
-    {
-       alert("Sides A, B, C make a Triangle");
-       if( triangle[0]=== triangle[1] &&  triangle[1]=== triangle[2] &&  triangle[2]=== triangle[0])
-       {
-       alert("The triangle is an EQUILATERAL Triangle");
-       }
-       else if(( triangle[0] ===  triangle[1])||( triangle[1] ===  triangle[2])||( triangle[2] ===  triangle[0]))
-       {
-         alert("The triangle is an ISOSCELE Triangle");
-       }
-       else 
-        {
-          alert("The triangle is an SCALENE Triangle");
-        }
-       }
-    else
-    {
-      alert("Sides A, B and C cannot make triangle"); 
-    }
-};
+//     if(( triangle[0]+ triangle[1])> triangle[2] && ( triangle[1] +  triangle[2])> triangle[0] && ( triangle[0] +  triangle[2])> triangle[1])
+//     {
+//        alert("Sides A, B, C make a Triangle");
+//        if( triangle[0]=== triangle[1] &&  triangle[1]=== triangle[2] &&  triangle[2]=== triangle[0])
+//        {
+//        alert("The triangle is an EQUILATERAL Triangle");
+//        }
+//        else if(( triangle[0] ===  triangle[1])||( triangle[1] ===  triangle[2])||( triangle[2] ===  triangle[0]))
+//        {
+//          alert("The triangle is an ISOSCELE Triangle");
+//        }
+//        else 
+//         {
+//           alert("The triangle is an SCALENE Triangle");
+//         }
+//        }
+//     else
+//     {
+//       alert("Sides A, B and C cannot make triangle"); 
+//     }
+// };
+</p>
