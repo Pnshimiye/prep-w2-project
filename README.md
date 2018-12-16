09a721124fe6fd0da48fc747e3d158b25e55f834
Pnshimiye <pnshimiye@gmail.com>
2018-12-17 00:09:52 +0200

    create an array for the triangle sizes

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Triangle tracker Application
 
-##  Description
+For this projet I will make a small application that will detect  if numbers entered by the user make a triangle,
+ and if they do what type of trisngle they make
+ 
+ ## Author
+ Done by Pauline Nshimiye a student at Moringa School
+
+ ## Setup Instructions
+
+ for you to be able to open this project and view its code you will need a Web Browser such as Goofle chrome
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-var sideA = document.getElementByIdsideA
-var sideB=(parseInt(prompt("enter number for side B")))
-var sideC=(parseInt(prompt("enter number for side C")))
+//This is a function that gets input from 3 text boxes and checks:
+//1. If the values can make a triange
+//2. What type of triangle they can make
+function makeTriangle()
+{
+//Declaration of an array that will contain the 3 values
+  var triangle =[sideA,sideB,sideC]
+  triangle[0]=parseInt(document.getElementById("sideA").value);
+  triangle[1]=parseInt(document.getElementById("sideB").value);
+  triangle[2]=parseInt(document.getElementById("sideC").value);
 
-var sideAsideB = (parseInt(prompt("enter number for side A")))+(parseInt(prompt("enter number for side B")))
-var sideAsideC = (parseInt(prompt("enter number for side A")))+ (parseInt(prompt("enter number for side C")))
-var sideBsideC =(parseInt(prompt("enter number for side B"))) + (parseInt(prompt("enter number for side C")))
+    if(( triangle[0]+ triangle[1])> triangle[2] && ( triangle[1] +  triangle[2])> triangle[0] && ( triangle[0] +  triangle[2])> triangle[1])
+    {
+       alert("Sides A, B, C make a Triangle");
+       if( triangle[0]=== triangle[1] &&  triangle[1]=== triangle[2] &&  triangle[2]=== triangle[0])
+       {
+       alert("The triangle is an EQUILATERAL Triangle");
+       }
+       else if(( triangle[0] ===  triangle[1])||( triangle[1] ===  triangle[2])||( triangle[2] ===  triangle[0]))
+       {
+         alert("The triangle is an ISOSCELE Triangle");
+       }
+       else 
+        {
+          alert("The triangle is an SCALENE Triangle");
+        }
+       }
+    else
+    {
+      alert("Sides A, B and C cannot make triangle"); 
+    }
+};
