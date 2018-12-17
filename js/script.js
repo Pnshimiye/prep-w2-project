481ee28771de9722ff11b23ee2109560803a9166
Pnshimiye <pnshimiye@gmail.com>
2018-12-17 08:25:45 +0200

    add  a nested if statement for an isocele triangle

--- js/script.js
+++ js/script.js
@@ -14,12 +14,12 @@ function makeTriangle()
        alert("Sides A, B, C make a Triangle");
     if( triangle[0]=== triangle[1] &&  triangle[1]=== triangle[2] &&  triangle[2]=== triangle[0])
          {
-            alert("The triangle is an EQUILATERAL Triangle");
-//        }
-//        else if(( triangle[0] ===  triangle[1])||( triangle[1] ===  triangle[2])||( triangle[2] ===  triangle[0]))
-//        {
-//          alert("The triangle is an ISOSCELE Triangle");
-//        }
+           
+            alert("The triangle is an EQUILATERAL Triangle");        }
+    else if(( triangle[0] ===  triangle[1])||( triangle[1] ===  triangle[2])||( triangle[2] ===  triangle[0]))
+      {
+        alert("The triangle is an ISOSCELE Triangle");
+      }
 //        else 
 //         {
 //           alert("The triangle is an SCALENE Triangle");
